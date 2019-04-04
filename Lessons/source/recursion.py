15b53f3deb46d345827213be4fef676e86833342
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-04-04 16:52:37 -0700

    [update] recursion

--- Lessons/source/recursion.py
+++ Lessons/source/recursion.py
@@ -14,12 +14,11 @@ def factorial(n):
 
 def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
-    # Use For loop or While loop
+    # 
     result = 1
     n_to_list = list(range(1, n+1))
     for num in n_to_list:
         result = result * num
-    print(result)
     return result
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
