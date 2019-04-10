92ae346fc2f1f9f5eac726aac2176ef1afecb2ba
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-04-10 00:08:43 -0700

    [update] Recursive Palidrome

--- Lessons/source/palindromes.py
+++ Lessons/source/palindromes.py
@@ -13,21 +13,14 @@ def is_palindrome(text):
     # implement is_palindrome_iterative and is_palindrome_recursive below, then
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
-    return is_palindrome_iterative(text)
-    # return is_palindrome_recursive(text)
+    # return is_palindrome_iterative(text)
+    return is_palindrome_recursive(text)
 
 
 def is_palindrome_iterative(text):
     # Input is a word 'racecar'
     # Output should determine if it is a palindrome
 
-    # Take the input text and turn it into a list of characters
-    # Reverse the list
-    # Compare that reversed list to the original list of charaters
-    # Check by seeing if they contain the same index
-    # Return true if so / if not Return false
-
-    # Second Iteration, Covers all test cases
     # Removes the need for punctuations
     no_punctuation = str.maketrans('', '', string.punctuation)
     text = text.translate(no_punctuation)
@@ -49,16 +42,38 @@ def is_palindrome_iterative(text):
     return True
 
 def is_palindrome_recursive(text, left=None, right=None):
-    # TODO: implement the is_palindrome function recursively here
-    pass
-    # once implemented, change is_palindrome to call is_palindrome_recursive
-    # to verify that your iterative implementation passes all tests
+    # Checks if the text is just one character
+    if len(text) <= 1:
+        return True
+    # Checks if text is two of the same charaters
+    if left is not None and left >= right:
+        return True
+    # Keeps track of the first and last index of the word
+    if left == None or right == None:
+        text = text.lower()
+        left = 0
+        right = len(text) - 1
+    # Increase the left value if it runs into a space or punctuations
+    while text[left] in string.punctuation + ' ':
+        left += 1
+    # Decrease the right value if it runs into a space or punctuations
+    while text[right] in string.punctuation + ' ':
+        right -= 1
+    # Return False if the first and last indexes to not match
+    if text[left] != text[right]:
+        return False
+    # Increment the first index and Decrease the last index to continue the while loop
+    left += 1
+    right -= 1
+    # Recursive Call
+    return is_palindrome_recursive(text, left, right)
+
 
 
 def main():
     import sys
     args = sys.argv[1:]  # Ignore script file name
-    print(is_palindrome('racecar'))
+    print(is_palindrome('noon'))
     if len(args) > 0:
         for arg in args:
             is_pal = is_palindrome(arg)
