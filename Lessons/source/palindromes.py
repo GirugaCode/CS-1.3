b5567702530f5e9ea49990d648406c40dc6cb4d6
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-04-09 18:55:45 -0700

    [add] Palindrome Iterative

--- Lessons/source/palindromes.py
+++ Lessons/source/palindromes.py
@@ -18,11 +18,35 @@ def is_palindrome(text):
 
 
 def is_palindrome_iterative(text):
-    # TODO: implement the is_palindrome function iteratively here
-    pass
-    # once implemented, change is_palindrome to call is_palindrome_iterative
-    # to verify that your iterative implementation passes all tests
+    # Input is a word 'racecar'
+    # Output should determine if it is a palindrome
 
+    # Take the input text and turn it into a list of characters
+    # Reverse the list
+    # Compare that reversed list to the original list of charaters
+    # Check by seeing if they contain the same index
+    # Return true if so / if not Return false
+
+    # Second Iteration, Covers all test cases
+    # Removes the need for punctuations
+    no_punctuation = str.maketrans('', '', string.punctuation)
+    text = text.translate(no_punctuation)
+    # Makes sure all texts are lowercase for the function
+    text = text.lower()
+    # Replaces all white spaces in text to none
+    text = text.replace(' ', '')
+    # Keeps track of the first and last index of the word
+    first_index = 0
+    last_index = len(text) - 1
+    # As long as the first index is less than the last index
+    while first_index < last_index:
+        # Return False if the first and last indexes to not match
+        if text[first_index] != text[last_index]:
+            return False
+        # Increment the first index and Decrease the last index to continue the while loop
+        first_index += 1
+        last_index -= 1
+    return True
 
 def is_palindrome_recursive(text, left=None, right=None):
     # TODO: implement the is_palindrome function recursively here
@@ -34,6 +58,7 @@ def is_palindrome_recursive(text, left=None, right=None):
 def main():
     import sys
     args = sys.argv[1:]  # Ignore script file name
+    print(is_palindrome('racecar'))
     if len(args) > 0:
         for arg in args:
             is_pal = is_palindrome(arg)
