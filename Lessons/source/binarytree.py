960887dc674339f2b89b501fd7a6aaa1eb944053
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-05-06 13:52:39 -0700

    [add] Traverse Tree

--- Lessons/source/binarytree.py
+++ Lessons/source/binarytree.py
@@ -244,7 +244,7 @@ class BinarySearchTree(object):
         Start at the given node and visit each node with the given function.
         TODO: Running time: ??? Why and under what conditions?
         TODO: Memory usage: ??? Why and under what conditions?"""
-        # Traverse left subtree, if it exists
+        # Traverse left subtree, if it exists  
         if node.left:
             self._traverse_in_order_recursive(node.left, visit)
         # Visit this node's data with given function
@@ -252,7 +252,7 @@ class BinarySearchTree(object):
         # Traverse right subtree, if it exists
         if node.right:
             self._traverse_in_order_recursive(node.right, visit)
-
+        
     def _traverse_in_order_iterative(self, node, visit):
         """Traverse this binary tree with iterative in-order traversal (DFS).
         Start at the given node and visit each node with the given function.
@@ -312,10 +312,6 @@ class BinarySearchTree(object):
             self._traverse_post_order_recursive(node.right, visit)
         # Visit this node's data with given function
         visit(node.data)
-
-
-
-
 
     def _traverse_post_order_iterative(self, node, visit):
         """Traverse this binary tree with iterative post-order traversal (DFS).
