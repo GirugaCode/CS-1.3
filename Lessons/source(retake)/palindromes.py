6a22ccbb8ad6d828eb6e3d81db30503a476054bc
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-06-08 20:00:03 -0700

    [update] Annotated palindromes

--- Lessons/source(retake)/palindromes.py
+++ Lessons/source(retake)/palindromes.py
@@ -13,15 +13,15 @@ def is_palindrome(text):
     # implement is_palindrome_iterative and is_palindrome_recursive below, then
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
-    return is_palindrome_iterative(text)
-    # return is_palindrome_recursive(text)
+    # return is_palindrome_iterative(text)
+    return is_palindrome_recursive(text)
 
 
-def is_palindrome_iterative(text):
-    # TODO: implement the is_palindrome function iteratively here
-    # once implemented, change is_palindrome to call is_palindrome_iterative
-    # to verify that your iterative implementation passes all tests
-    
+def is_palindrome_iterative(text): 
+    """
+    Runtime: O(n)
+    Condition: Where n is the number of text we have to go through
+    """   
     left = 0
     right = len(text) - 1
     
@@ -48,10 +48,11 @@ def is_palindrome_iterative(text):
 
 
 def is_palindrome_recursive(text, left=None, right=None):
-    # TODO: implement the is_palindrome function recursively here
-    pass
-    # once implemented, change is_palindrome to call is_palindrome_recursive
-    # to verify that your iterative implementation passes all tests
+    """
+    Runtime: O(n)
+    Condition: Where n is the number of text we have to go through
+    """   
+
     # Checks if the text is just one character
     if len(text) <= 1:
         return True
