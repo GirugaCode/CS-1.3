2a42f5f832497eadac9181c0d9c4d5a90913f8ce
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-06-08 18:00:42 -0700

    [add] Palindrome Recursive

--- Lessons/source(retake)/palindromes.py
+++ Lessons/source(retake)/palindromes.py
@@ -52,7 +52,31 @@ def is_palindrome_recursive(text, left=None, right=None):
     pass
     # once implemented, change is_palindrome to call is_palindrome_recursive
     # to verify that your iterative implementation passes all tests
-
+    # Checks if the text is just one character
+    if len(text) <= 1:
+        return True
+    # Checks if text is two of the same charaters
+    if left is not None and left >= right:
+        return True
+    # Keeps track of the first and last index of the word
+    if left == None or right == None:
+        text = text.lower()
+        left = 0
+        right = len(text) - 1
+    # Increase the left value if it runs into a space or punctuations
+    while text[left] in string.punctuation + ' ':
+        left += 1
+    # Decrease the right value if it runs into a space or punctuations
+    while text[right] in string.punctuation + ' ':
+        right -= 1
+    # Return False if the first and last indexes to not match
+    if text[left] != text[right]:
+        return False
+    # Increment the first index and Decrease the last index to continue the while loop
+    left += 1
+    right -= 1
+    # Recursive Call
+    return is_palindrome_recursive(text, left, right)
 
 def main():
     import sys
