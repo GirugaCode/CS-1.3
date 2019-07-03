93f5436d53c1c946f2af70fe0e5a123f11de19ed
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-07-02 19:35:07 -0700

    [add] Time complexity for set operations

--- Lessons/source(retake)/sets.py
+++ Lessons/source(retake)/sets.py
@@ -41,7 +41,9 @@ class Set():
         self.size -= 1
     
     def union(self, other_set):
-        """ Returns a set that has that is a unity of two seperate sets"""
+        """ Returns a set that has that is a unity of two seperate sets
+        Runtime: O(n) where n is the amount of items we have to search through to add to the set
+        """
         union_set = Set()
         for key, value in self.items:
             union_set.add(key)
@@ -50,7 +52,9 @@ class Set():
         return union_set
 
     def intersection(self, other_set):
-        """Returns a set that has common elements between two sets"""
+        """Returns a set that has common elements between two sets
+        Runtime: O(n) where n is the amount of items we have to search through to add to the set
+        """
         intersection_set = Set()
         
         for key, value in self.items:
@@ -59,7 +63,9 @@ class Set():
         return intersection_set
 
     def difference(self, other_set):
-        """Returns the unique elements in the first set"""
+        """Returns the unique elements in the first set
+        Runtime: O(n) where n is the amount of items we have to search through to add to the set
+        """
         difference_set = Set()
 
         for key, value in self.items:
@@ -68,7 +74,9 @@ class Set():
         return difference_set
 
     def is_subset(self, other_set):
-        """Checks if the set contains a part of the other set"""
+        """Checks if the set contains a part of the other set
+        Runtime: O(n) where n is the amount of items we have to search through to check the items
+        """
         for key, value in self.items:
             if not other_set.contains(key):
                 return False
