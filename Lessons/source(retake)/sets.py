ef9154e0b854c4e343a1b5f92119f9b34e38d4e1
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-07-02 19:21:18 -0700

    [update] Annotated runtime for basic operations

--- Lessons/source(retake)/sets.py
+++ Lessons/source(retake)/sets.py
@@ -13,19 +13,29 @@ class Set():
                 self.add(item)
     
     def length(self):
-        """Calculates the length of the set."""
+        """Calculates the length of the set.
+        Runtime: O(n) where n is the number of times we iterate through the buckets
+        to check the length
+        """
         return self.items.length()
     
     def contains(self, element):
-        """Checks if the element is in the set."""
+        """Checks if the element is in the set.
+        Runtime: O(1) constant time because we are just checking the value and 
+        using the hash function to check the bucket index
+        """
         return self.items.contains(element)
 
     def add(self, element):
-        """Adds an element to the set."""
+        """Adds an element to the set.
+        Runtime: O(1) constant time on average to hash the value and place it in a corresponding bucket
+        """
         self.items.set(element, element) # Think of the data as a tuple, they both point to the same place in memeory
         self.size += 1
     
     def remove(self, element):
-        """Removes an element from the set."""
+        """Removes an element from the set.
+        Runtime: O(1) constant time to find the bucket and delete the item
+        """
         self.items.delete(element)
         self.size -= 1
