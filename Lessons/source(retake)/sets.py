0190c059aaa64f8dca8f9cee0d3c7cf2ea3933ad
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-07-02 19:32:27 -0700

    [add] Set operations

--- Lessons/source(retake)/sets.py
+++ Lessons/source(retake)/sets.py
@@ -39,3 +39,37 @@ class Set():
         """
         self.items.delete(element)
         self.size -= 1
+    
+    def union(self, other_set):
+        """ Returns a set that has that is a unity of two seperate sets"""
+        union_set = Set()
+        for key, value in self.items:
+            union_set.add(key)
+        for key, value in other_set.items:
+            union_set.add(key)
+        return union_set
+
+    def intersection(self, other_set):
+        """Returns a set that has common elements between two sets"""
+        intersection_set = Set()
+        
+        for key, value in self.items:
+            if other_set.contains(key):
+                intersection_set.add(key)
+        return intersection_set
+
+    def difference(self, other_set):
+        """Returns the unique elements in the first set"""
+        difference_set = Set()
+
+        for key, value in self.items:
+            if not other_set.contains(key):
+                difference_set.add(key)
+        return difference_set
+
+    def is_subset(self, other_set):
+        """Checks if the set contains a part of the other set"""
+        for key, value in self.items:
+            if not other_set.contains(key):
+                return False
+        return True
